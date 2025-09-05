99a57cda17b54c2ae81b2d0286ea72a91c511b94
wirkelzirkel <andreasgruber@magenta.de>
2025-09-05 10:09:44 +0200

    neuroevolution meta-pipeline

--- utils.py
+++ utils.py
@@ -26,9 +26,9 @@ def save_coordinates_to_csv(
         print(f"An unexpected error occurred: {e}")
 
 def setup_folder_structure(output_dir: str):
-    if not os.path.exists(f"output"):
-        os.mkdir("output")
     if not os.path.exists(f"{output_dir}/data_analysis"):
         os.mkdir(f"{output_dir}/data_analysis")
     if not os.path.exists(f"{output_dir}/coordinates"):
         os.mkdir(f"{output_dir}/coordinates")
+    if not os.path.exists(f"{output_dir}/video"):
+        os.mkdir(f"{output_dir}/video")
